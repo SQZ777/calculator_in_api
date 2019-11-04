72030d00dee539ce3df636c7c479b11d8e6beade
sqz777 <sqz777isme@gmail.com>
2019-11-04 20:44:26 +0800

    init project

---
+++ TestCalculator/TestCalculatorService.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCalculator
+{
+    [TestClass]
+    public class TestCalculatorService
+    {
+        [TestMethod]
+        public void Input_null_and_null_Should_Return_Error()
+        {
+        }
+    }
+}
