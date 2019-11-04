2d184f0a991ae7892e623f88cf1ede8eefac9627
sqz777 <sqz777isme@gmail.com>
2019-11-04 21:50:13 +0800

    pass 1st controller add test case

--- CalculatorInApi/Controllers/CalculatorController.cs
+++ CalculatorInApi/Controllers/CalculatorController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CalculatorInApi.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CalculatorInApi.Controllers
 {
     public class CalculatorController : Controller
     {
-        public IActionResult Index()
+        private ICalculatorService _calculatorService;
+        public CalculatorController(ICalculatorService calculatorService)
         {
-            return View();
+            this._calculatorService = calculatorService;
+        }
+
+        public IActionResult Add(int num1, int num2)
+        {
+
+            if (_calculatorService.CheckNumber(num1, num2))
+            {
+                return Ok(_calculatorService.Add(num1, num2));
+            }
+            throw new NotImplementedException();
         }
     }
 }
---
+++ TestCalculator/TestCalculatorControllerAdd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CalculatorInApi.Controllers;
+using CalculatorInApi.Interfaces;
+using CalculatorInApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TestCalculator
+{
+    [TestClass]
+    public class TestCalculatorControllerAdd
+    {
+        [TestMethod]
+        public void Add_0_and_0_Should_be_0()
+        {
+            var num1 = 0;
+            var num2 = 0;
+            var mockCalculatorService = new Mock<ICalculatorService>();
+            mockCalculatorService.Setup(x => x.CheckNumber(num1, num2)).Returns(true);
+            mockCalculatorService.Setup(x => x.Add(num1, num2)).Returns(0);
+            var calculatorController = new CalculatorController(mockCalculatorService.Object);
+
+            var actual = calculatorController.Add(num1, num2) as OkObjectResult;
+            var expect = new OkObjectResult(0);
+
+            Assert.AreEqual(expect.Value, actual.Value);
+        }
+    }
+}
