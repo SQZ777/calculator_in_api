26f987ac4aff9c9f2c01bc1e322d64ebabfe487e
sqz777 <sqz777isme@gmail.com>
2019-11-04 20:56:18 +0800

    refactor test code

--- TestCalculator/TestCalculatorService.cs
+++ TestCalculator/TestCalculatorService.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using CalculatorInApi.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,26 +10,25 @@ namespace TestCalculator
         [TestMethod]
         public void Add_Input_0_and_0_Should_Return_0()
         {
-            var calculatorService = new CalculatorService();
-            var num1 = 0;
-            var num2 = 0;
-            var actual = calculatorService.Add(num1, num2);
 
+            var actual = CalculatorServiceAdd(0, 0);
             var expect = 0;
-            
             Assert.AreEqual(expect, actual);
         }
 
         [TestMethod]
         public void Add_Input_1_and_1_Should_Return_2()
         {
-            var calculatorService = new CalculatorService();
-            var num1 = 1;
-            var num2 = 1;
-            var actual = calculatorService.Add(num1, num2);
-
+            var actual = CalculatorServiceAdd(1, 1);
             var expect = 2;
             Assert.AreEqual(expect, actual);
+        }
+
+        private int CalculatorServiceAdd(int num1, int num2)
+        {
+            var calculatorService = new CalculatorService();
+            var actual = calculatorService.Add(num1, num2);
+            return actual;
         }
     }
 }
