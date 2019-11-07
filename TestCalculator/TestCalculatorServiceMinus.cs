321aeb37910dd7c84eb460a72d163e8a1c75e389
sqz777 <sqz777isme@gmail.com>
2019-11-07 22:19:43 +0800

    pass 2nd calculatorService minus test case

--- TestCalculator/TestCalculatorServiceMinus.cs
+++ TestCalculator/TestCalculatorServiceMinus.cs
@@ -19,5 +19,16 @@ namespace TestCalculator
             var expect = 0;
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void Minus_m1_and_1_Should_Be_m2()
+        {
+            var num1 = -1;
+            var num2 = 1;
+            var calculatorService = new CalculatorService();
+            var actual = calculatorService.Minus(num1, num2);
+            var expect = -2;
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
