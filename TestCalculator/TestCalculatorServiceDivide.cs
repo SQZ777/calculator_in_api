883279e122e2b8dfae7f9d22a2b1e29abf3626e6
sqz777 <sqz777isme@gmail.com>
2019-11-08 01:13:47 +0800

    add test cases: divide in service

--- TestCalculator/TestCalculatorServiceDivide.cs
+++ TestCalculator/TestCalculatorServiceDivide.cs
@@ -26,5 +26,23 @@ namespace TestCalculator
             var expect = "0.33";
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void Divide_3_1_Should_3_00()
+        {
+            var calculatorService = new CalculatorService();
+            var actual = calculatorService.Divide(3, 1);
+            var expect = "3.00";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Divide_1_0_Should_unlimited_symbol()
+        {
+            var calculatorService = new CalculatorService();
+            var actual = calculatorService.Divide(1, 0);
+            var expect = "∞";
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
