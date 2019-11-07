634faa009cb0eab02bec982d76595893417f5500
sqz777 <sqz777isme@gmail.com>
2019-11-07 20:42:51 +0800

    fail 2nd minus controller test case

--- TestCalculator/TestCalculatorControllerMinus.cs
+++ TestCalculator/TestCalculatorControllerMinus.cs
@@ -22,6 +22,17 @@ namespace TestCalculator
             var actual = calculatorController.Minus(1, 1) as OkObjectResult;
             var expect = new OkObjectResult(0);
             Assert.AreEqual(expect.Value, actual.Value);
+        }
+
+        [TestMethod]
+        public void Minus_m2147483648_and_1_Should_Be_BadRequest_InputIsIllegally()
+        {
+            var mockService = new Mock<ICalculatorService>();
+            mockService.Setup(x => x.CheckNumber(-2147483648, 1)).Returns(false);
+            var calculatorController = new CalculatorController(mockService.Object);
+            var actual = calculatorController.Minus(-214783648, 1) as BadRequestObjectResult;
+            var expect = new BadRequestObjectResult("InputIsIllegally");
+            Assert.AreEqual(expect.Value, actual.Value);
 
         }
     }
