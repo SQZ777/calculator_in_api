fe71e8d20ae783f3f530aa6ee60c94a08cbf703a
sqz777 <sqz777isme@gmail.com>
2019-11-08 01:15:46 +0800

    fail 1st test case: checkNumberInDivide in service

---
+++ TestCalculator/TestCalculatorServiceCheckNumberInDivide.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CalculatorInApi.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCalculator
+{
+    [TestClass]
+    public class TestCalculatorServiceCheckNumberInDivide
+    {
+        [TestMethod]
+        public void CheckNumberInDivide_1_1_Should_Be_true()
+        {
+            var calculatorService = new CalculatorService();
+            var actual = calculatorService.CheckNumberInDivide(1, 1);
+            Assert.IsTrue(actual);
+        }
+    }
+}
