7a40459a779fa17bca0134ad7ffe5a57500c6375
sqz777 <sqz777isme@gmail.com>
2019-11-07 22:17:39 +0800

    fail 1st calculator service minus test case

---
+++ TestCalculator/TestCalculatorServiceMinus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CalculatorInApi.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCalculator
+{
+    [TestClass]
+    public class TestCalculatorServiceMinus
+    {
+        [TestMethod]
+        public void Minus_1_and_1_Should_Be_0()
+        {
+            var num1 = 1;
+            var num2 = 1;
+            var calculatorService = new CalculatorService();
+            var actual = calculatorService.Minus(num1, num2);
+            var expect = 0;
+            Assert.AreEqual(expect, actual);
+
+        }
+    }
+}
