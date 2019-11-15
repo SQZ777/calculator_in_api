d5fb2c6a34d5c3af267bdf785c378ecc8914c528
sqz777 <sqz777isme@gmail.com>
2019-11-15 11:24:45 +0800

    build success

--- CalculatorInApi/Startup.cs
+++ CalculatorInApi/Startup.cs
@@ -27,7 +27,8 @@ namespace CalculatorInApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddScoped<ICalculatorService, CalculatorService>();
         }
 
